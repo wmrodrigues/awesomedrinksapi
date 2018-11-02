8382bd927bf61a8784c168e27c0e998759e6d8fa
wmrodrigues <washington.moises@gmail.com>
2018-11-02 17:37:46 -0300

    feature/develop Including project files.

---
+++ src/test/java/com/awesomedrinksapi/AwesomedrinksapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.awesomedrinksapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwesomedrinksapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
